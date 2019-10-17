2dd69567d9efbe9aa74c9c92f9cc81abcb2bdf15
Naillik13 <killian.galea0@orange.fr>
2019-10-17 09:11:34 +0200

    Random Beer

--- App.js
+++ App.js
@@ -1,19 +1,29 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { createAppContainer } from 'react-navigation';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
+import { createStackNavigator } from 'react-navigation-stack';
+import HomeScreen from "./src/screens/HomeScreen";
+import RandomBeerScreen from "./src/screens/RandomBeerScreen";
+import DetailsScreen from "./src/screens/DetailsScreen";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+const HomeStack = createStackNavigator({
+    Home: HomeScreen,
+    Details: DetailsScreen,
 });
+
+const RandomStack = createStackNavigator({
+    Random: RandomBeerScreen,
+    Details: DetailsScreen,
+});
+
+export default createAppContainer(
+    createBottomTabNavigator(
+        {
+            Home: HomeStack,
+            Random: RandomStack,
+        },
+        {
+            /* Other configuration remains unchanged */
+        }
+    )
+);
